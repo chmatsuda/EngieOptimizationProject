1175d72b3f2886d7b8de0556976a2492459056d2
Jen Kim <54411575+7kimsangmi@users.noreply.github.com>
2021-03-31 10:04:55 -0400

    Add files via upload
    With costing includedc

--- BatterySizing_1.m
+++ BatterySizing_1.m
@@ -1,4 +1,5 @@
 %% Load data and plot
+
 clear all;
 load('ExcelImported2.mat');
 %%
@@ -6,6 +7,7 @@ t = [1:8760]';
 ElecDemand = table2array(ElecDemand);%kW
 RenewableEnergy = table2array(RenewableEnergy);
 %% Can we use a battery for a 2MW + 4.5 MW max PV distribution center if the peak is 3.9MW
+
 PVInstalled = 4500; %How many kW of PV are installed, adjust this accordingly
 WindInstalled = 0; %3.6MW of Wind installed = 3600kW 
 
@@ -14,8 +16,8 @@ PV = RenewableEnergy(:,1)*PVInstalled; %Kw
 Wind = RenewableEnergy(:,2)*WindInstalled; %Kw
 %TotalRen = PV+Wind; %kW
 PVnGrid = PV+GridProduction;
-
 %% Plotting the production
+
 figure(1)
 plot(t,ElecDemand,'b',t,PV,'g',t,GridProduction,'r');
 legend("Demand","PV","Grid");
@@ -27,17 +29,19 @@ legend("Demand","PV + Grid");
 ylabel('Power(kW)')
 %% Compute the energy that is consumed over the time period and the energy produced:
 % E = P * t
+
 %Assuming that each time step is exactly 60 seconds:
 %Each time step is 1 hour = 3600 s
-energy_consumed = sum(ElecDemand)* 3600/1000/1000%% kW converted to G
-PV_energy_prod = sum(PV) *3600/1000/1000 %%GJ
-Grid_energy_prod = sum(GridProduction) *3600/1000/1000 %GJ
+energy_consumed = sum(ElecDemand)*3600/10^6;%% kJ converted to GJ
+PV_energy_prod = sum(PV) *3600/10^6; %%GJ
+Grid_energy_prod = sum(GridProduction) *3600/10^6; %GJ
 total_energy_prod = PV_energy_prod + Grid_energy_prod;
 
 %The energy consumed is 80,209 GJ and energy produced with PV + 2MW grid is
-%83,020 GJ so hypothetically could work...
-
+%80,804 GJ so hypothetically could work... for the yearly production/needs.
+%Doesn't account for instantaneous needs.
 %% What percentage of time is it self sufficient?
+
 %If we don't have a battery
 sstime = 0;
 PowerStillNeeded = zeros(8760,1);
@@ -49,19 +53,23 @@ for i = 1:8760
     end
     
 end
-percentage = sstime/8760
-% 51.31 of the time, it is self sufficient
-
+percentage = sstime/8760.;
+X = sprintf('The system is self-sufficient for %d percent of the time',percentage);
+disp(X)
+% 51.31% of the time, it is self sufficient
 %% Max Power of Battery in Discharge Mode in MW
+
 %Above, we found the difference in demand and production in the variable
 %PowerStillNeeded.
 
 
 %Finding the maximum difference between production and demand,
-MaxPower= max(PowerStillNeeded)/1000 %MW
-%Gives 1.573 MW as the max
-
+MaxPower= max(PowerStillNeeded)/1000; %MW
+X = sprintf('Maximum battery capacity needed at a given moment: %d MW',MaxPower);
+disp(X)
+%Gives 1.5725 MW as the max
 %% Max Size of Battery in MWh
+
 for k = 1:15000
     Capacities = linspace(1,15000,15000);
     sstime2=0; %Time With Power
@@ -75,7 +83,7 @@ for k = 1:15000
             sstime2 = sstime2 + 1;
           elseif PowerDiff <0
             EnergyNeeded = abs(PowerDiff); %%MWh
-            if EnergyNeeded < BatteryE
+            if EnergyNeeded <= BatteryE
                 sstime2 = sstime2+ 1;
             elseif BatteryE < EnergyNeeded
                 if BatteryE >0
@@ -91,25 +99,75 @@ for k = 1:15000
     percentage2 = sstime2/8760;
     suff(k) = percentage2;
     caps(k) = Capacities(k);
-    percentage2
-    Capacities(k)
+    percentage2;
+    Capacities(k);
     if percentage2 >= PercentWanted
-        disp(percentage2)
-        disp(Capacities(k))
+        X = sprintf('Percentage of time in which demand is covered by supply: %d percent', percentage2*100);
+        disp(X)
+        CapacityNeeded = Capacities(k);
+        X = sprintf('Total battery capacity needed: %d MWh', CapacityNeeded);
+        disp(X)
+        
         break;
     end
     
     
     
 end
-
 %%
 figure(3)
 plot(suff,caps)
 ylabel('Needed Capacity (MWh)')
 xlabel('Percent self sufficient')
+%% Cost
+% Battery CAPEX
 
 
+LiIonCAPEX = CapacityNeeded*300e3; % 300 euros for per kWh of capacity
+fprintf('Battery CAPEX: %d euros',LiIonCAPEX)
+%% 
+% Cost of purchasing from grid
+
+CommodityPrices = table2array(CommodityPrices);%kW
+MarketPrice = CommodityPrices(:,1); %euros/kWh
+PurchasedElectricty = zeros(8760);
+for i = 1:8760
+    CostOn = MarketPrice(i)*2e3 ; %assuming 2MW is continuously supplied
+    PurchasedElectricity(i) = CostOn;
+end
+PurchasedElectricity = sum(PurchasedElectricity);    
+X = sprintf('Cost of electricity from grid: %d euros',PurchasedElectricity);
+disp(X)
+%% 
+% Cost of charging battery and profit from selling excess PV production
+
+BatteryChargeHeld = zeros(8760,1);
+MaxCap = CapacityNeeded*10^3; % Max capacity of battery set to capacity needed in kWh
+BatteryChargeHeld(1) = MaxCap; % Assume we start with a full battery.
+BatteryChargeCost = zeros(8760,1);
+ExcessSale = zeros(8760,1);
+for j = 1:8760
+    BatteryChargeHeld(j+1) = BatteryChargeHeld(j)+PV(j)+GridProduction(j)-ElecDemand(j);
+    if BatteryChargeHeld(j+1) <= MaxCap
+        BatteryChargeCost(j) = PV(j)*20.5; % 20.5 euros per kWh charge 
+        ExcessSale(j) = 0; % if battery charge held < max capacity, then no excess PV to be sold
+    else
+        BatteryChargeCost(j) = (MaxCap - BatteryChargeHeld(j))*20.5; % 20.5 euros per kWh charge 
+        ExcessSale(j) = (BatteryChargeHeld(j)+PV(j) - MaxCap)*0.3*MarketPrice(j); % selling at 30% of commodity price
+        BatteryChargeHeld(j+1) = MaxCap;
+    end
+end
+BatteryChargeCost = sum(BatteryChargeCost);
+X = sprintf('Annual cost of charging battery: %d euros',BatteryChargeCost);
+disp(X)
+ExcessSale = sum(ExcessSale);
+Y = sprintf('Annual money made from selling excess energy produced at 30 percent market price: %d euros', ExcessSale);
+disp(Y)
+ChargeatYearEnd = BatteryChargeHeld(8760)
+%% 
+% Total Costs for 2021-2024
+
+TotalCost = LiIonCAPEX + PurchasedElectricity + BatteryChargeCost - ExcessSale
 %%
 function BatteryEnergy = ChargeDrawBat(BatteryEnergy, PowerDifference, MaxCap)
     EnergyDiff = PowerDifference;
@@ -122,5 +180,3 @@ function BatteryEnergy = ChargeDrawBat(BatteryEnergy, PowerDifference, MaxCap)
         end
     
 end
-
-
